61124a730a636ec31dd13e5b0c29f157920365c9
Eugenio Paolantonio (g7) <me@medesimo.eu>
2023-04-09 20:39:11 +0200

    [linux-android-fxtec-pro1x] Disable global_pid_lookups in cmdline
    It's a better idea to keep this around.
    
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -11,7 +11,7 @@ VARIANT = android
 KERNEL_BASE_VERSION = 4.19-0
 
 # The kernel cmdline to use
-KERNEL_BOOTIMAGE_CMDLINE = console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x4a90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 buildvariant=userdebug console=tty0 droidian.lvm.prefer
+KERNEL_BOOTIMAGE_CMDLINE = console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x4a90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 buildvariant=userdebug console=tty0 binder.global_pid_lookups=0 droidian.lvm.prefer
 
 # Slug for the device vendor. This is going to be used in the KERNELRELASE
 # and package names.
